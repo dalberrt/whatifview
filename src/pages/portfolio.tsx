56b942cbdce43a0365bdfe2956ab657b7ca0dc5f
dalberrt <dalbertchea@gmail.com>
2026-02-01 14:43:48 +0800

    fix: button

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button"
 import { ChartLineLinear } from "@/components/ui/chart-line-linear"
 import { ChartLineMultiple } from "@/components/ui/chart-line-multiple"
 
 function Portfolio() {
     return (
     <main className="min-h-screen bg-background text-foreground flex flex-col items-center justify-center p-8">
-        <Button>Hi there!</Button>
+        <h1>Portfolio</h1>
         <ChartLineLinear />
         <ChartLineMultiple />
     </main>
