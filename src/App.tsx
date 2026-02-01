62a2f5f1ea72b3e189a4e04073e6529893bd9fb5
dalberrt <dalbertchea@gmail.com>
2026-02-01 13:42:52 +0800

    add: chart-line-multiple

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Button } from "@/components/ui/button"
 import { ChartLineLinear } from "@/components/ui/chart-line-linear"
+import { ChartLineMultiple } from "@/components/ui/chart-line-multiple"
 import './App.css'
 
 function App() {
@@ -19,12 +20,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <Button>Hi there!</Button>
         <ChartLineLinear />
+        <ChartLineMultiple />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
